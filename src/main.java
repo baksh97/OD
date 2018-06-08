829798390b24d249419cd5757d07046eb2f3b38c
Kashish Bansal <e0302940@nus.edu.sg>
2018-06-08 18:09:02 +0800

    started.. doubt in time array..

---
+++ src/main.java
@@ -0,0 +1,113 @@
+import java.util.List;
+
+public class main {
+	
+	int clusterNums[];
+	List<cluster> clusters;
+	
+	int timeWindow = 10;
+	
+	
+	double findTimeBetweenNodeAtTime(node n1,node n2, double currentTime) {
+		
+		cluster cluster1 = getCluster(n1);
+		cluster cluster2 = getCluster(n2);
+		
+		List<node> bdryPointsCluster1 = cluster1.getBdry_points();
+		List<node> bdryPointsCluster2 = cluster2.getBdry_points();
+		
+		for(node bdryPt: bdryPointsCluster1) {
+			
+			int timeIndex1 = (int) (currentTime%timeWindow);
+			
+			double t1 = cluster1.getTimeFromNodeToBdry().get(timeIndex1).get(n1.getClusterId()).get(bdryPt.getClusterId());
+			
+			int timeIndex2 = (int) ((currentTime+t1)%timeWindow);
+			
+			for(node otherBdryPt: bdryPointsCluster2) {
+				double t2 = cluster1.getTimeBtwBdry().get(timeIndex2).get(bdryPt.getClusterId()).get(otherBdryPt.getClusterId());				
+			}
+		}
+	}
+	
+	cluster getCluster(node n) {
+		int id = n.getId();
+		return clusters.get(clusterNums[id]);
+	}
+	
+	public static void main(String args[]) {
+		System.out.println((int)3.444%2);
+	}
+	
+}
+
+
+class node{
+	private int id,clusterId;
+	private int clusterNum;
+	
+	boolean isBdryPoint;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getClusterId() {
+		return clusterId;
+	}
+
+	public void setClusterId(int clusterId) {
+		this.clusterId = clusterId;
+	}
+
+	public int getClusterNum() {
+		return clusterNum;
+	}
+
+	public void setClusterNum(int clusterNum) {
+		this.clusterNum = clusterNum;
+	}
+
+	public boolean isBdryPoint() {
+		return isBdryPoint;
+	}
+
+	public void setBdryPoint(boolean isBdryPoint) {
+		this.isBdryPoint = isBdryPoint;
+	}
+}
+
+class cluster{
+	
+	private List<node> bdryPoints;
+	private List<List<List<Double>>> timeFromNodeToBdry;
+	private List<List<List<Double>>> timeBtwBdry;
+
+	public List<List<List<Double>>> getTimeFromNodeToBdry() {
+		return timeFromNodeToBdry;
+	}
+
+	public void setTimeFromNodeToBdry(List<List<List<Double>>> timeFromNodeToBdry) {
+		this.timeFromNodeToBdry = timeFromNodeToBdry;
+	}
+
+	public List<List<List<Double>>> getTimeBtwBdry() {
+		return timeBtwBdry;
+	}
+
+	public void setTimeBtwBdry(List<List<List<Double>>> timeBtwBdry) {
+		this.timeBtwBdry = timeBtwBdry;
+	}
+
+	List<node> getBdry_points(){
+		return bdryPoints;
+	}
+	
+	void setBdry_points(List<node> bdryPoints) {
+		this.bdryPoints = bdryPoints;
+	}
+}
